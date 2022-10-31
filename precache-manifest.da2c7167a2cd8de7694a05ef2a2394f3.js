727032b4f41044a547eb1e59733e6a9a6769656e
icesix233 <11140788+icesix233@user.noreply.gitee.com>
2022-10-31 17:50:44 +0800

    Updates

---
+++ precache-manifest.da2c7167a2cd8de7694a05ef2a2394f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddf49a15fbd9e819cade596375cbaee7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78b488a75ef8f038e5b5",
+    "url": "/static/css/main.aa3f5b09.chunk.css"
+  },
+  {
+    "revision": "84b205a4a27ef7a41484",
+    "url": "/static/js/2.9fe6875c.chunk.js"
+  },
+  {
+    "revision": "679dc0b0298984b03ab372d214526e9a",
+    "url": "/static/js/2.9fe6875c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78b488a75ef8f038e5b5",
+    "url": "/static/js/main.34db5040.chunk.js"
+  },
+  {
+    "revision": "ae2dfafbfc2365c4ffff",
+    "url": "/static/js/runtime-main.f7c5443e.js"
+  }
+]);
